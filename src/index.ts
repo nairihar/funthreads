4b419a0ece36d002ae794a2cc9f26bf943ed264f
Nairi Harutyunyan <nairi.haroutiounian@gmail.com>
2023-08-06 23:50:08 +0400

    feat: Support default and single function import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import startWorker from './worker/worker';
 import { createWorkerMetadata } from './worker/metadata';
 
-const executeInThread = (task: () => any, ...params: any[]) => {
+export const executeInThread = (task: () => any, ...params: any[]) => {
   const metadata = createWorkerMetadata(task, params);
   return startWorker(metadata);
 };
